ece246168c6da8c23e73a83b1140b291beb77d93
Thomas.Elsgaard <thomas.elsgaard@mooserocket.dk>
2023-11-21 12:38:57 +0100

    initial commit

---
+++ utils.go
@@ -0,0 +1,16 @@
+package mooseutils
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func PrettyPrint(data interface{}) {
+	var p []byte
+	p, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s \n", p)
+}
